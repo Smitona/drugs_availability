4e17efa27fb936196e3bf32c2ad8a6cbe0a71c36
Smitona <jarofsmetana@yandex.ru>
2024-03-21 21:04:46 +0300

    Bot talking

--- parser/parser_spb.py
+++ parser/parser_spb.py
@@ -1,4 +1,26 @@
 # https://eservice.gu.spb.ru/portalFront/resources/portal.html#medicament
 
+import requests
 from bs4 import BeautifulSoup
 
+
+class DrugsParser:
+
+    def __init__(self) -> None:
+        self.page = page
+        self.user = None
+        self.site_url = URL
+
+    async def send_request(self):
+        link = await self.build_link()
+        response = requests.get(link)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'lxml')
+            available = soup.find_all()
+
+
+class DrugsUser:
+
+    def __init__(self) -> None:
+        pass
