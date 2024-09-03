d65ff9f07347decee65c24d57371b3928add083c
Smitona <jarofsmetana@yandex.ru>
2024-09-03 23:34:46 +0300

    function for parsing drug_forms

--- parse/parse/spiders/drugs_spider.py
+++ parse/parse/spiders/drugs_spider.py
@@ -26,7 +26,7 @@ class DrugsSpider(scrapy.Spider):
 
         yield scrapy.Request(url=self.url, callback=self.parse)
 
-    async def build_url(self):
+    async def build_url(self) -> str:
         options: SearchOption = self.user.search_options
         self.options = options
 
@@ -36,12 +36,12 @@ class DrugsSpider(scrapy.Spider):
             f"&ost{options.type_of_privilege}=true"
         )
 
-    async def parse(self, response):
+    async def click_auth_button(self, response):
         link = await self.build_url()
         response = requests.get(link)
 
         if response.status_code == 200:
-            # Кликнуть кнопку «Продолжить без авторизации», чтобы получить ответ
+            # Кликнуть кнопку «Продолжить без авторизации», чтобы получить ответ страницы
             self.driver.get(response.url)
             auth_button = self.driver.find_element_by_xpath(
                 '//*[@id="common-popup"]/div/div[1]/div/div[3]/div/div[1]/button'
@@ -50,26 +50,40 @@ class DrugsSpider(scrapy.Spider):
             if auth_button is not None:
                 auth_button.click()
 
-            drug_forms = response.css('.droppanel__head-title::text').extract()
-            return drug_forms
+    async def get_drug_forms(self, response):
+        response = HtmlResponse(url=self.build_url(), body=body, encoding="utf-8")
 
+        drug_forms = Selector(response=response).xpath(
+            "//*[@id="medicament-search-result"]/div/div/button/span"
+        ).getall()
+
+        if drug_form is None:
+            return 'Препарата нет в наличии.'
+        else:
+            # Вернуть список для выбора формы препарата
+            return enumerate(drug_forms)
 
 @dataclass
 class SearchOption:
+    """Параметры поиска для препаратов."""
     def __init__(
             self, drug_name: str,
             type_of_privilege: int,
             drug_form: int,
-        ):
+    ):
         self.drug_name = drug_name
         self.type_of_privilege = type_of_privilege
+        self.drug_form = drug_form
 
 
 class DrugsUser:
+    """Пользователь бота."""
 
     def __init__(
             self, drug_name: str,
             type_of_privilege: int,
             drug_form: int,
-        ) -> None:
-        self.search_options = SearchOption(drug_name, type_of_privilege)
+    ) -> None:
+        self.search_options = SearchOption(
+            drug_name, type_of_privilege, drug_form
+        )
