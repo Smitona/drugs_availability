4275c1e13f299aef7a7e79bf2c459ea29998d04c
Smitona <jarofsmetana@yandex.ru>
2025-04-02 00:03:11 +0300

    two inline follow buttons added

--- bot/bot.py
+++ bot/bot.py
@@ -26,14 +26,14 @@ router = Router()
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    Handler for messages '/start' command
-    """
-
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton(
                 text='Поиск препарата', callback_data='search_drug'
+            )],
+            [InlineKeyboardButton(
+                text='Избранные препараты',
+                callback_data='favorite_drugs'
             )]
         ]
     )
@@ -46,11 +46,35 @@ async def command_start_handler(message: Message) -> None:
     )
 
 
-@router.callback_query(lambda c: c.data == "search_drug")
+@router.callback_query(lambda c: c.data == 'search_drug')
 async def callback_search_drug_handler(
     callback_query: types.CallbackQuery
 ) -> None:
     await callback_query.message.answer("Введите название препарата для поиска.")
+    await callback_query.answer()
+
+
+@router.callback_query(lambda c: c.data == 'favorite_drugs')
+async def callback_favorite_drugs(
+    callback_query: types.CallbackQuery
+) -> None:
+
+    # Добавить select drug.name и drug.dosage для кнопок у польза.
+    # callback должен отдавать ответ из кеша/БД по препарату
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(
+                text='Ранвэк', callback_data='ранвэк'
+            )],
+            [InlineKeyboardButton(
+                text='Пентаса', callback_data='пентаса'
+            )]
+        ]
+    )
+
+    await callback_query.message.edit_text(
+        text="Выберите препарат", reply_markup=keyboard
+    )
     await callback_query.answer()
 
 
@@ -60,7 +84,9 @@ async def handle_user_input(message: Message) -> None:
     drug = str(message.text)
     result = json.dumps(make_request(drug), ensure_ascii=False, indent=2)
 
-    await message.answer(result)
+    formatted_result = f"<pre>{result}</pre> Попробуйте повторить поиск позже."
+
+    await message.answer(formatted_result, parse_mode="HTML")
 
 
 async def main() -> None:
